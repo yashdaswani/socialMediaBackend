2b4fb6fd4d43875ea77b89a4b1d03529492f3d81
Yash Kumar Daswani <100758924+yashdaswani@users.noreply.github.com>
2024-02-29 17:34:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,11 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-  origin: ['http://localhost:3000'],
+  origin: "*",
   methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 }));
-
 
 app.use(cookieParser());
 app.use(express.json());
